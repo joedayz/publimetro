1eaa040ea45fc315fc6ee4e68e0c598fa18128d1
Jose Diaz <jose.diaz@joedayz.pe>
2016-02-13 21:37:19 -0500

    creando version inicial de splash

---
+++ Publimetro/app/src/main/java/pe/joedayz/publimetro/controller/SeleccionaUbicacionActivity.java
@@ -0,0 +1,16 @@
+package pe.joedayz.publimetro.controller;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import pe.joedayz.publimetro.R;
+
+public class SeleccionaUbicacionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecciona_ubicacion);
+    }
+
+}
